d44a7298445b6445ac48552dc214ef0373e0b92b
Cyrille <cypierre58@gmail.com>
2016-12-02 13:57:32 +0100

    [Log] Creating log library

---
+++ test/src/log.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <util/log.hpp>
+
+namespace log =  util::log;
+
+void test() {
+    log::Logger l;
+    l.prefix("test(): ");
+    l << "fromage";
+    l << "champignon";
+}
+
+int main() {
+//    log::setStream(std::cout);
+//    log::format() << log::item::time << log::item::msg;
+
+    log::cfg().timeEnabled(true);
+    log::cfg().logLevel(log::msg);
+
+    log::Logger logger;
+    logger.prefix("main(): ");
+    logger(log::dbg) << "cheval " << "mouton " << 8 << ' ' << 4.3;
+    logger(log::msg) << "les brouettes c'est pratique";
+    logger(log::warn) << "la maison n°" << 27 << " est bleu";
+    test();
+    logger(log::err) << "niveau important de message";
+    logger(log::crit) << "attention ça va exploser";
+    logger << "et comme ça ?";
+}
