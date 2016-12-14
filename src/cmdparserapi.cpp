332df7891598507e168aa2d5445c94af4edb8548
Alexis Pereda <alexis@pereda.fr>
2016-12-14 19:47:31 +0100

    [server] update serverAPI to work with events

--- src/cmdparserapi.cpp
+++ src/cmdparserapi.cpp
@@ -9,146 +9,139 @@
 namespace elog = ese::log;
 
 CmdParserAPI::CmdParserAPI(Player & player) : CmdParserBase{player} {
-    _lg.prefix("cmdParserAPI: ");
+	_lg.prefix("cmdParserAPI: ");
 }
 
 std::string CmdParserAPI::add(std::istringstream & iss) {
-    std::string id;
-    iss >> id;
-    nlohmann::json json;
+	std::string id;
+	iss >> id;
+	nlohmann::json json;
 
-    if (id.size() < cfg::ytIdSize) {
-        std::string err{"Invalid video ID : '" + id + "'"};
-        _lg(elog::warn) << err;
-        json["error"] = err;
-    } else {
-        try {
-            id = id.substr(id.size() - cfg::ytIdSize);
-            std::string title(_yt.getVideoTitle(cfg::ytUrlPrefix + id));
-            _lg(elog::dbg) << "video title = \"" << title << '"';
-            _player.add(id, title);
-            json["id"] = id;
-            json["title"] = title;
-        }
-        catch (UnknownVideo const & e) {
-            json["error"] = std::string{e.what()};
-        }
-    }
+	if (id.size() < cfg::ytIdSize) {
+		std::string err{"Invalid video ID : '" + id + "'"};
+		_lg(elog::warn) << err;
+		json["error"] = err;
+	} else {
+		try {
+			id = id.substr(id.size() - cfg::ytIdSize);
+			std::string title(_yt.getVideoTitle(cfg::ytUrlPrefix + id));
+			_lg(elog::dbg) << "video title = \"" << title << '"';
+			_player.add(id, title);
+			return "";
+		}
+		catch (UnknownVideo const & e) {
+			json["error"] = std::string{e.what()};
+		}
+	}
 
-    return json.dump() + "\n";
+	return json.dump() + "\n";
 }
 
 std::string CmdParserAPI::list(std::istringstream & iss) {
-    std::size_t nbLines;
-    Player::PlayListView const * plv;
-    if (iss >> nbLines) plv = &_player.list(nbLines);
-    else plv = &_player.list();
+	std::size_t nbLines;
+	Player::PlayListView const * plv;
+	if (iss >> nbLines) plv = &_player.list(nbLines);
+	else plv = &_player.list();
 
-    nlohmann::json json = nlohmann::json::array();
+	nlohmann::json json;
+	json["event"] = -1;
+	json["value"] = nlohmann::json::array();
 
-    int i = 0;
-    for (auto const & it : *plv) {
-        nlohmann::json item = nlohmann::json::object();
-        item["index"] = i++;
-        item["id"] = it->id();
-        item["title"] = it->title();
-        _lg(elog::trace) << "list += [" << it->id() << "] " << it->title();
-        json += item;
-    }
-    return json.dump() + "\n";
+	int i = _player.hasCurrent();
+	if(i) {
+		auto const current = _player.current();
+		auto const it = &current;
+		nlohmann::json item = nlohmann::json::object();
+		item["index"] = i++;
+		item["id"] = it->id();
+		item["title"] = it->title();
+		_lg(elog::trace) << "list += [" << it->id() << "] " << it->title();
+		json["value"] += item;
+	}
+
+	for (auto const & it : *plv) {
+		nlohmann::json item = nlohmann::json::object();
+		item["index"] = i++;
+		item["id"] = it->id();
+		item["title"] = it->title();
+		_lg(elog::trace) << "list += [" << it->id() << "] " << it->title();
+		json["value"] += item;
+	}
+	return json.dump() + "\n";
 }
 
 std::string CmdParserAPI::rm(std::istringstream & iss) {
-    nlohmann::json json;
-    std::string id;
-    if (iss >> id) {
-        char * end;
-        std::size_t index = std::strtoul(id.c_str(), &end, 10);
-        if (!*end) {
-            auto wm = _player.remove(index);
-            if (wm) json["id"] = id;
-        }
-        else if (id.size() == cfg::ytIdSize){
-            auto wm = _player.remove(id);
-            if (wm) json["id"] = id;
-        } else {
-					json["error"] = "remove failed";
-					_lg(elog::warn) << "remove failed (id = " << id << ')';
-				}
+	nlohmann::json json;
+	std::string id;
+	if (iss >> id) {
+		char * end;
+		std::size_t index = std::strtoul(id.c_str(), &end, 10);
+		if (!*end) {
+			_player.remove(index);
+			return "";
+		} else if (id.size() == cfg::ytIdSize) {
+			_player.remove(id);
+			return "";
+		} else {
+			json["error"] = "remove failed";
+			_lg(elog::warn) << "remove failed (id = " << id << ')';
 		}
-    else {
-        json["error"] = "remove failed";
-        _lg(elog::warn) << "Remove failed: parsing error";
-    }
-    return json.dump() + "\n";
+	}
+	else {
+		json["error"] = "remove failed";
+		_lg(elog::warn) << "Remove failed: parsing error";
+	}
+	return json.dump() + "\n";
 }
 
 std::string CmdParserAPI::clear(std::istringstream &) {
-    nlohmann::json json;
-    _player.clear();
-    json["message"] = "clear";
-    return json.dump() + "\n";
+	_player.clear();
+	return "";
 }
 
 std::string CmdParserAPI::next(std::istringstream &) {
-    nlohmann::json json;
-    _player.next();
-    json["message"] = "next";
-    return json.dump() + "\n";
+	_player.next();
+	return "";
 }
 
 std::string CmdParserAPI::pause(std::istringstream &) {
-    nlohmann::json json;
-    _player.togglePause();
-    json["message"] = "pause";
-    json["playing"] = !_player.isPaused();
-    return json.dump() + "\n";
+	_player.togglePause();
+	return "";
 }
 
 std::string CmdParserAPI::volume(std::istringstream & iss) {
-    Player::Volume value;
-    nlohmann::json json;
+	nlohmann::json json;
+	Player::Volume value;
 
-    if (iss >> value)
-        json["volume"] = _player.incrVolume(value);
-    else
-        json["volume"] = _player.volume();
-    return json.dump() + "\n";
+	json["event"] = 3;
+	if (iss >> value) {
+		_player.incrVolume(value);
+		return "";
+	} else
+		json["value"] = std::to_string(_player.volume());
+	return json.dump() + "\n";
 }
 
 std::string CmdParserAPI::progress(std::istringstream & iss) {
-    nlohmann::json json;
-    json["current"] = _player.timePos();
-    json["total"] = _player.duration();
-    return json.dump() + "\n";
+	nlohmann::json json;
+	json["event"] = -2;
+	json["value"]["current"] = _player.timePos();
+	json["value"]["total"] = _player.duration();
+	return json.dump() + "\n";
 }
 
 std::string CmdParserAPI::current(std::istringstream &) {
-		nlohmann::json json;
-		if (_player.hasCurrent()) {
-				WebMusic wm{_player.current()};
-				json["id"] = wm.id();
-				json["title"] = wm.title();
-		} else
-				json["message"] = "No current music";
-		return json.dump() + "\n";
+	return "";
 }
 
 std::string CmdParserAPI::state(std::istringstream &) {
-		nlohmann::json json;
-		json["hasCurrent"] = _player.hasCurrent();
-		json["paused"] = _player.isPaused();
-        return json.dump() + "\n";
+	nlohmann::json json;
+	json["event"] = 4;
+	json["value"] = std::to_string(_player.isPaused());
+	return json.dump() + "\n";
 }
 
 std::string CmdParserAPI::random(std::istringstream &) {
-    nlohmann::json json;
-    auto wm = _player.addRandom();
-    if (wm) {
-        json["id"] = wm->id();
-        json["title"] = wm->title();
-    } else
-        json["error"] = "Archive is empty";
-
-    return json.dump() + "\n";
+	_player.addRandom();
+	return "";
 }
--- src/eventviewer.hpp
+++ src/eventviewer.hpp
@@ -4,23 +4,39 @@
 #include <log/log.hpp>
 #include "player.hpp"
 #include <iostream>
+#include "nlohmann/json.hpp"
 
 namespace view {
 
 using EventWriter =  std::function<std::string(util::Any)>;
 
 std::array<EventWriter, 6> const eventWriters {
-    [] (util::Any a) { return "Adding: " + a.cast<WebMusic>().title(); },
-    [] (util::Any a) { return "Removing: " + a.cast<WebMusic>().title(); },
-    [] (util::Any a) { return "Playing: " + a.cast<WebMusic>().title(); },
-    [] (util::Any a) { return "New volume: " + std::to_string(a.cast<double>()) + "%"; },
-    [] (util::Any a) { return a.cast<bool>() ? "Player paused" : "Player resumed."; },
-    [] (util::Any)   { return "Playlist cleared."; },
+	[] (util::Any a) { return "Adding: " + a.cast<WebMusic>().title(); },
+	[] (util::Any a) { return "Removing: " + a.cast<WebMusic>().title(); },
+	[] (util::Any a) { return "Playing: " + a.cast<WebMusic>().title(); },
+	[] (util::Any a) { return "New volume: " + std::to_string(a.cast<double>()) + "%"; },
+	[] (util::Any a) { return a.cast<bool>() ? "Player paused" : "Player resumed."; },
+	[] (util::Any)	 { return "Playlist cleared."; },
 };
 
+std::array<EventWriter, 6> const eventWritersAPI {
+	[] (util::Any a)	{ return a.cast<WebMusic>().id(); },
+	[] (util::Any a)	{ return a.cast<WebMusic>().id(); },
+	[] (util::Any a)	{ return a.cast<WebMusic>().id(); },
+	[] (util::Any a)	{ return std::to_string(a.cast<double>()); },
+	[] (util::Any a)	{ return std::to_string(a.cast<bool>()); },
+	[] (util::Any)		{ return ""; }
+};
 
 std::string eventFormat(PlayerEvt evt, util::Any a) {
-    return eventWriters[evt](std::move(a));
+	return eventWriters[evt](std::move(a));
+}
+
+std::string eventFormatAPI(PlayerEvt evt, util::Any a) {
+	nlohmann::json json;
+	json["event"] = evt;
+	json["value"] = eventWritersAPI[evt](std::move(a));
+	return json.dump()+"\n";
 }
 
 }
